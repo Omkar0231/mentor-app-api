f3153b9fce2535a61076042eacff39fa27e7da76
Tanish8923 <tg6398119861@gmail.com>
2025-03-12 02:19:41 +0530

    Add accounts app with custom user model

--- MentorApp/settings.py
+++ MentorApp/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
+
+# Custom User Model
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
