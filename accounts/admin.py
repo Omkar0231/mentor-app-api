416d1aa4160fb4fcbb31b4e1d4daf2204ab5dc11
Tanish8923 <tg6398119861@gmail.com>
2025-03-20 05:49:06 +0530

    add email otp verification

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,20 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser , OTPVerification
+
+@admin.register(OTPVerification)
+class OTPVerificationAdmin(admin.ModelAdmin):
+    list_display = ('email', 'otp', 'created_at')
+    search_fields = ('email', 'otp')
+
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('phone_number', 'age')}),
+    list_display = ("email", "first_name", "last_name", "role", "password", "is_staff", "is_superuser" )
+    list_filter = ("role", "is_staff", "is_superuser", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "role", "age")}),   
+        ("Permissions", {"fields": ("is_staff", "is_superuser", "is_active", "groups", "user_permissions")}),
     )
 
-    # Show custom fields in the list display
-    list_display = ('email', 'role', 'phone_number', 'age', 'is_staff', 'is_active')
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "first_name", "last_name", "role", "is_staff", "is_superuser", "is_active"),
+        }),
+    )
 
-    # Add filters for role and is_staff
-    list_filter = ('role', 'is_staff', 'is_active')
-
-    # Add search functionality
-    search_fields = ('email', 'phone_number', 'first_name', 'last_name')
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)  # Change from 'username' to 'email'
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import RegisterView , LoginView 
+from .views import RegisterView , LoginView , VerifyOTPView , ResetPasswordView , VerifyResetOTPView , ForgotPasswordView
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
-    path('signin/', LoginView.as_view(), name='signin'),  # 
+    path('verify-otp/', VerifyOTPView.as_view(), name='verify_otp'),
+    path('signin/', LoginView.as_view(), name='signin'),  
+    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'), 
+    path('verify-reset-otp/', VerifyResetOTPView.as_view(), name='verify-reset-otp'), 
+    path('reset-password/', ResetPasswordView.as_view(), name='reset-password'), 
 ]
